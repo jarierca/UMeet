3a7561f533907117eada8b42aad947ebf5a8b2e7
Aritz Iribarren <a24898@svalero.com>
2021-09-14 12:15:32 +0200

    arreglos varios

---
+++ src/main/java/com/umeet/umeet/entities/Rol.java
@@ -0,0 +1,24 @@
+package com.umeet.umeet.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nickName;
+    private String email;
+    private String pass;
+    private String avatar;
+    private String status;
+}
