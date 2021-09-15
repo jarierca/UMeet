36ba8295629e33cc716256df21ee01b97b7bc44b
Marcos Gimeno <marcosgimeno@gmail.com>
2021-09-15 09:44:43 +0200

    Merge origin/master into marcosg
    Conflicts:
    	src/main/java/com/jorgerubira/apuntes/@Resumen.txt

--- src/main/java/com/umeet/umeet/controller/FriendController.java
+++ src/main/java/com/umeet/umeet/controller/FriendController.java
@@ -1,6 +1,6 @@
 package com.umeet.umeet.controller;
 
-import com.umeet.umeet.services.IFriendService;
+import com.umeet.umeet.interfaces.IFriendService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/com/umeet/umeet/services/FriendService.java
@@ -0,0 +1,37 @@
+package com.umeet.umeet.services;
+
+import com.umeet.umeet.interfaces.IFriendService;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FriendService implements IFriendService{
+    private List<String> nombres=new ArrayList<>();
+
+    
+    /*
+    @Override
+    public void altaNombre(String nombre) throws OperacionEnListaException {
+        if (nombre==null){ //Desde el exterior se debe comprobar si vale nulo.
+            throw new NullPointerException();
+        }
+        if (nombres.stream().noneMatch(x->x.equals(nombre))){
+            nombres.add(nombre);
+        }else{
+            throw new OperacionEnListaException(nombre);
+        }
+    }
+
+    @Override
+    public void bajaNombre(String nombre) {
+        nombres.remove(nombre);
+    }
+*/
+    @Override
+    public List<String> getFriendList() {
+        //Devuelve una lista inmutable.
+        return Collections.unmodifiableList(nombres);
+    }
+}
