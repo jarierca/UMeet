b9d428cfcf23dd5193e91e099d33b90a87b09259
Aran <aranzazu_90@hotmail.es>
2021-09-15 13:24:48 +0200

    servidor por usuario, todos  los servidores, filtra servidor por nombre o descripcion OK

--- src/main/java/com/umeet/umeet/controller/ServerUserController.java
+++ src/main/java/com/umeet/umeet/controller/ServerUserController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.umeet.umeet.repositories.ServersRepository;
+import com.umeet.umeet.repositories.UserRepository;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -21,8 +22,17 @@ public class ServerUserController {
 
     @Autowired
     private ServersRepository repoServer;
+    @Autowired
+    private UserRepository repoUser;
 
-    //mostrar todos los servidores
+    @GetMapping("/byUser")
+    public String serverByUser(Model m, Long userId) {
+
+        m.addAttribute("user", repoUser.findById(userId).get());
+
+        return "/servers/byUser";
+    }
+
     @GetMapping
     public String allServers(Model m) {
 
@@ -33,20 +43,19 @@ public class ServerUserController {
 
     @PostMapping("/filtered")
     public String searchServer(Model m, String name) {
-        
-       List <Server> aux = repoServer.findByNameContaining(name);
-        if(!aux.isEmpty()){
-             m.addAttribute("nam", aux);
+
+        List<Server> aux = repoServer.findByNameContaining(name);
+        if (!aux.isEmpty()) {
+            m.addAttribute("nam", aux);
         }
-        List <Server> aux1 = repoServer.findByDescriptionContaining(name);
-        if(!aux1.isEmpty()){
-             m.addAttribute("des", aux1);
+        List<Server> aux1 = repoServer.findByDescriptionContaining(name);
+        if (!aux1.isEmpty()) {
+            m.addAttribute("des", aux1);
         }
+
         
-        
-         //System.out.println( repoServer.findByNameContaining(name));
         return "servers/filteredServers";
-       
+
     }
 
 }
---
+++ src/main/java/com/umeet/umeet/repositories/UserServerRoleRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.umeet.umeet.repositories;
+
+import com.umeet.umeet.entities.UserServerRole;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Aran
+ */
+public interface UserServerRoleRepository extends JpaRepository<UserServerRole, Long>{
+    
+    
+    
+}
