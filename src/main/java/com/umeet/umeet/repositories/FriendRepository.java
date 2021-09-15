b722ac054cdd13a411d2719c19c5c512aa8b4914
Marcos Gimeno <marcosgimeno@gmail.com>
2021-09-15 08:43:31 +0200

    Merge origin/master into marcosg
    Conflicts:
    	src/main/java/com/jorgerubira/apuntes/@Resumen.txt

--- src/main/java/com/umeet/umeet/repositories/FriendRepository.java
+++ src/main/java/com/umeet/umeet/repositories/FriendRepository.java
@@ -3,12 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.umeet.umeet.interfaces;
+package com.umeet.umeet.repositories;
+
+import com.umeet.umeet.entities.Friend;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
  * @author skudo
  */
-public interface IUsuarios {
+public interface FriendRepository extends JpaRepository<Friend, Long>{
     
 }
---
+++ src/main/java/com/umeet/umeet/services/IFriendService.java
@@ -0,0 +1,13 @@
+package com.umeet.umeet.services;
+
+import java.util.List;
+
+
+public interface IFriendService {
+    
+    /**
+     * Devuelve una lista con los nombres.
+     */
+    public List<String> getFriendList();
+    
+}
