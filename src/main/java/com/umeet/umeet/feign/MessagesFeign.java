906585cddd18e475bf73a17ffcfac6767a3a7054
Jose <martosmartinjose@gmail.com>
2021-09-29 14:42:27 +0200

    as

--- src/main/java/com/umeet/umeet/feign/MessagesFeign.java
+++ src/main/java/com/umeet/umeet/feign/MessagesFeign.java
@@ -1,10 +1,13 @@
 package com.umeet.umeet.feign;
 
 import com.umeet.umeet.dtos.MessageChannelDto;
+import com.umeet.umeet.entities.Message;
+import com.umeet.umeet.entities.MessageFile;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(/*name = "UMeetBack"*/name = "invent", url= "http://localhost:8082")
 @RequestMapping("/b/msg")
@@ -15,4 +18,10 @@ public interface MessagesFeign {
     
     @PostMapping("/private/{id_destino}") 
     public List<MessageChannelDto> privados(@PathVariable Long id_destino, @RequestParam Long idUser);
+    
+    @PostMapping("/channel/sendmsg") 
+    public void mensajeCanal(Message msg,Long idChannel,Long idUser);
+
+    @PostMapping("/private/sendmsg") 
+    public void mensajePrivado(Message msg,Long idUserDestiny, Long idUser);
 }
