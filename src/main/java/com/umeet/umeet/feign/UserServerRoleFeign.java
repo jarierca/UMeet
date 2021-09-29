d65fdb49f77344eedb23dd57a1d3195e6511f8bd
Jose <martosmartinjose@gmail.com>
2021-09-29 19:30:05 +0200

    Usr back listo

---
+++ src/main/java/com/umeet/umeet/feign/UserServerRoleFeign.java
@@ -0,0 +1,20 @@
+package com.umeet.umeet.feign;
+
+import com.umeet.umeet.entities.Server;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@FeignClient(contextId = "feignUsr", name = "UMeetBack", url= "http://localhost:8082")
+@RequestMapping("/b/usr")
+public interface UserServerRoleFeign {
+    
+    @PostMapping("/joinServer")
+    public Server joinServer(@RequestParam Long idServer, @RequestParam Long idUser);
+    
+    @PostMapping("/leaveServer")
+    public void leaveServer(@RequestParam Long idServer, @RequestParam Long idUser);
+    
+}
