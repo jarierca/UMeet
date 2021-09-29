f9c75a0d64374561b3a0510fabdd4c63dd37287a
Aran <aranzazu_90@hotmail.es>
2021-09-29 14:01:23 +0200

    category

--- src/main/java/com/umeet/umeet/controller/CategoryController.java
+++ src/main/java/com/umeet/umeet/controller/CategoryController.java
@@ -1,7 +1,6 @@
 package com.umeet.umeet.controller;
 
 import com.umeet.umeet.entities.Category;
-import com.umeet.umeet.entities.Channel;
 import com.umeet.umeet.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,10 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @Controller
-@RequestMapping("/category")
+@RequestMapping("/b/category")
 public class CategoryController {
 
     @Autowired
@@ -31,19 +31,32 @@ public class CategoryController {
     @Autowired
     MessageFileRepository messageFileRepository;
 
-    @GetMapping("/form")
-    public String viewCategoryCreation(Model model, Long idCategory, Long idServer){
+    /* @GetMapping("/form")
+    public String viewCategoryCreation(Model model, Long idCategory, Long idServer) {
         Category category = new Category();
-        if(idCategory==null){
+        if (idCategory == null) {
             category.setServer(serverRepository.findById(idServer).get());
         } else {
             category = categoryRepository.findById(idCategory).get();
         }
         model.addAttribute("category", category);
         return "formCategory";
+    }*/
+    
+    @GetMapping("/form")
+    public Category viewCategoryCreation(Model model, Long idCategory, Long idServer) {
+        Category category = new Category();
+        if (idCategory == null) {
+            category.setServer(serverRepository.findById(idServer).get());
+        } else {
+            category = categoryRepository.findById(idCategory).get();
+        }
+        //model.addAttribute("category", category);
+        return category;
     }
 
-    @PostMapping("/addCategory")
+
+    /*@PostMapping("/addCategory")
     public String addCategory(Category category, Long idServer){
         category.setServer(serverRepository.findById(idServer).get());
         if(category.getName()==null || ("").equals(category.getName())){
@@ -51,13 +64,29 @@ public class CategoryController {
         }
         categoryRepository.save(category);
         return "redirect:server/one?idServer="+category.getServer().getId();
+    }*/
+    @PostMapping("/addCategory")
+    public Category addCategory(Category category, Long idServer) {
+        category.setServer(serverRepository.findById(idServer).get());
+        if (category.getName() == null || ("").equals(category.getName())) {
+
+        }
+        categoryRepository.save(category);
+        return category;
+
     }
 
-    @GetMapping("/deleteCategory")
-    public String deleteCategory(Long idCategory){
+    /* @GetMapping("/deleteCategory")
+    public String deleteCategory(Long idCategory) {
         long idServer = categoryRepository.findById(idCategory).get().getServer().getId();
         categoryRepository.deleteById(idCategory);
-        return "redirect:server/one?idServer="+idServer;
+        return "redirect:server/one?idServer=" + idServer;
+    }*/
+    @GetMapping("/deleteCategory")
+    public void deleteCategory(Long idCategory) {
+        long idServer = categoryRepository.findById(idCategory).get().getServer().getId();
+        categoryRepository.deleteById(idCategory);
+        //return "redirect:server/one?idServer=" + idServer;
     }
 
 }
