b6a741d30a7131e0a99da9b38f0a081808bcbe58
Javier A <javialiaga2000@gmail.com>
2021-10-02 16:38:41 +0200

    mails

---
+++ src/main/java/com/umeet/UmeetEmailApplication.java
@@ -0,0 +1,13 @@
+package com.umeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UmeetEmailApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmeetEmailApplication.class, args);
+    }
+}
---
+++ src/main/java/com/umeet/controller/EmailController.java
@@ -0,0 +1,65 @@
+
+package com.umeet.controller;
+
+import java.io.IOException;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/m")
+public class EmailController {
+    
+    @Autowired
+    private JavaMailSender javaMailSender;
+    
+    @GetMapping("/mail")
+    void sendEmail(String email, String subject, String text) {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(email);
+
+        msg.setSubject(subject);
+        msg.setText(text);
+
+        javaMailSender.send(msg);
+
+    }
+
+    @GetMapping("/mailAttach")
+    void sendEmailWithAttachment(String email, String subject, String text, String file) throws MessagingException, IOException {
+
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        // true = multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+        helper.setTo(email);
+
+        helper.setSubject(subject);
+
+        // default = text/plain
+        //helper.setText("Check attachment for image!");
+
+        // true = text/html
+        helper.setText(text, true);
+
+        //FileSystemResource file = new FileSystemResource(new File("classpath:android.png"));
+
+        //Resource resource = new ClassPathResource("android.png");
+        //InputStream input = resource.getInputStream();
+
+        //ResourceUtils.getFile("classpath:android.png");
+
+        helper.addAttachment("my_photo.png", new ClassPathResource("android.png"));
+
+        javaMailSender.send(msg);
+
+    }
+}
