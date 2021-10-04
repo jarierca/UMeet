21c40c0977edd8bbf6575ff9f1886b96d7210974
JaviA <javialiaga2000@gmail.com>
2021-10-04 13:28:35 +0200

    text msg left

--- src/main/resources/static/javascript/funcionalidad.js
+++ src/main/resources/static/javascript/funcionalidad.js
@@ -184,7 +184,7 @@ function chat(idCanal, channelName) {
                 if(pJson[c].messageFile != null){
                     if(pJson[c].messageFile.name =="fichero"){
                         if (pJson[c].messageFile.url.toLowerCase().includes(".png") || pJson[c].messageFile.url.toLowerCase().includes(".jpg")){
-                            fichero = "<br /> <img src='/msg/download?url="+ x.messageFile.url+"' class='text-center' style='max-width:50%' />";   
+                            fichero = "<a href='/msg/download?url="+ x.messageFile.url+"' target='_blank' title='Descargar' class='tips'><i class='fas fa-file-download'></i></a> <br /> <img src='/msg/download?url="+ x.messageFile.url+"' class='text-center' style='max-width:50%' />";   
                         }else{
                             fichero = "<a href='/msg/download?url="+ x.messageFile.url+"' target='_blank' title='Descargar' class='tips'><i class='fas fa-file-download' style='font-size:35px;'></i></a>";   
                         }
@@ -304,7 +304,7 @@ function chatPrivado(idDestino, nameDestino) {
                 if(pJson[c].messageFile != null){
                     if(pJson[c].messageFile.name =="fichero"){
                         if (pJson[c].messageFile.url.toLowerCase().includes(".png") || pJson[c].messageFile.url.toLowerCase().includes(".jpg")){
-                            fichero = "<br /> <img src='/msg/download?url="+ x.messageFile.url+"' class='text-center' style='max-width:50%' />";   
+                            fichero = "<a href='/msg/download?url="+ x.messageFile.url+"' target='_blank' title='Descargar' class='tips'><i class='fas fa-file-download'></i></a> <br /> <img src='/msg/download?url="+ x.messageFile.url+"' class='text-center' style='max-width:50%' />";   
                         }else{
                             fichero = "<a href='/msg/download?url="+ x.messageFile.url+"' target='_blank' title='Descargar' class='tips'><i class='fas fa-file-download' style='font-size:35px;'></i></a>";   
                         }
