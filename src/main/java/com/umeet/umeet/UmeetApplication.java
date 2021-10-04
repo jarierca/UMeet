3b1190ae79e5eb62bc4191538032ca49ebbb7772
Jose <martosmartinjose@gmail.com>
2021-10-04 14:52:02 +0200

    Pagina de error añadida y ajustes pre-animado

--- src/main/java/com/umeet/umeet/UmeetApplication.java
+++ src/main/java/com/umeet/umeet/UmeetApplication.java
@@ -1,9 +1,12 @@
 package com.umeet.umeet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class UmeetApplication {
 
 	public static void main(String[] args) {
