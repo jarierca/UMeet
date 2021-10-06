c9d97850b1f1c388d04b2f90e68d19c81ea9a77c
Aritz Iribarren <a24898@svalero.com>
2021-10-06 12:57:42 +0200

    limpieza de codigo

--- src/main/java/com/umeet/umeet/controller/AccessController.java
+++ src/main/java/com/umeet/umeet/controller/AccessController.java
@@ -29,50 +29,9 @@ public class AccessController {
     
     @Autowired
     private UserRepository userRepository;
-    
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    
-    @Autowired
-    private FriendRepository friendRepo;
-    
-    @Autowired
-    private UserServerRoleRepository usrRepo;
 
     @Autowired
     private ModelMapper mapper;
-    
-    @GetMapping("/login")
-    public String login(){ 
-        Authentication auth= SecurityContextHolder.getContext().getAuthentication();
-        if (auth.getPrincipal() != "anonymousUser") {
-            return "redirect:home";
-        }else{
-            return "login";
-        }
-    }
-    
-//    @GetMapping("/register")
-//    public String register(Model m){
-//        Authentication auth= SecurityContextHolder.getContext().getAuthentication();
-//        if (auth.getPrincipal() != "anonymousUser") {
-//            return "redirect:home";
-//        }else{
-//            m.addAttribute("user", new User());
-//            return "register";
-//        }
-//    }
-    
-    @GetMapping("/info")
-    @ResponseBody
-    public String info(){
-        Authentication auth= SecurityContextHolder.getContext().getAuthentication();
-        if (auth!=null){
-            return auth.getName() + auth.getDetails().toString();
-        }else{
-            return "Nadie";
-        }
-    } 
     
     @PostMapping("/newregister")
     public Boolean newregister(UserDto userDto){
@@ -90,44 +49,5 @@ public class AccessController {
         }else{
             return false;
         }
-    }
-    
-    @GetMapping("/home")
-    public String index(HttpServletResponse response , Model m){
-        Authentication auth= SecurityContextHolder.getContext().getAuthentication();
-        if (auth.getPrincipal() != "anonymousUser"){
-            
-            String username = auth.getName(); 
-            UserValidacionDto u = (UserValidacionDto) auth.getPrincipal();
-            Optional<User> user = userRepository.findByUsername(username);              //Obtenemos lista de amigos e invitado para index
-            m.addAttribute("friendsAccepted", friendRepo.findByAmigos(u.getId(), "aceptado"));
-            m.addAttribute("friendsPending", friendRepo.findByAmigos(u.getId(), "invitado"));
-
-            List<UserServerRole> usrAux = usrRepo.findByUser(user.get());               //Obtenemos lista de servidores por usuario
-            List<Server> servers = usrAux.stream().map(x-> x.getServer()).collect(Collectors.toList());
-            m.addAttribute("userServers",servers);
-            
-        }
-        
-        return "index";
-    }
-    
-    @GetMapping("/log-out")
-    public String logoOut(HttpServletResponse response){
-        Authentication auth= SecurityContextHolder.getContext().getAuthentication();
-        if (auth.getPrincipal() != "anonymousUser"){
-            String username = auth.getName();
-            User user = userRepository.findByUsername(username).get();
-            user.setStatus("desconectado");
-            userRepository.save(user);
-        } 
-        System.out.println("\n\nEBNTRAZQA\n\n");
-        
-        logout();
-        return "redirect:logout";
-    }
-    @GetMapping("/logout")
-    public String logout(){
-        return "login";
     }
 }
